9aa2fd0331fdf9ecb29b485a4650ea4465f0bbae
Spyros Mouchlianitis <spirosgsaaa@gmail.com>
2022-01-07 17:45:19 +0200

    Initial commit

---
+++ src/main/java/com/hua/gr/DS_Assignment/DsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hua.gr.DS_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAssignmentApplication.class, args);
+	}
+
+}
