604c9f7b2fb4f8717a42923f5332966e1541579f
Apos_Dimit <aposdimit@gmail.com>
2022-01-12 13:38:24 +0200

    IDK

--- src/main/java/com/hua/gr/DS_Assignment/DsAssignmentApplication.java
+++ src/main/java/com/hua/gr/DS_Assignment/DsAssignmentApplication.java
@@ -2,6 +2,9 @@ package com.hua.gr.DS_Assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DsAssignmentApplication {
@@ -9,5 +12,7 @@ public class DsAssignmentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DsAssignmentApplication.class, args);
 	}
+
+	
 
 }
