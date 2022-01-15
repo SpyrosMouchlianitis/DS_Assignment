13325889537f7cb18bcac5e9004e82c88647caf4
Apos_Dimit <aposdimit@gmail.com>
2022-01-15 18:24:36 +0200

    Build user system

--- src/main/java/com/hua/gr/DS_Assignment/UserCatalog.java
+++ src/main/java/com/hua/gr/DS_Assignment/UserCatalog.java
@@ -1,51 +1,74 @@
 package com.hua.gr.DS_Assignment;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_catalog")
 public class UserCatalog {
-    private Citizen citizen;
-    private UserAdmin userAdmin;
-    private Employee employee;
 
-    public Citizen getCitizen() {
-        return citizen;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Id
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    //Employee has number 0
+    //Boss has number 1
+    @Column(name = "permission")
+    private Integer permission;
+
+    public Integer getPermission() {
+        return permission;
     }
 
-    public void setCitizen(Citizen citizen) {
-        this.citizen = citizen;
+    public void setPermission(Integer permission) {
+        this.permission = permission;
     }
 
-    public UserAdmin getUserAdmin() {
-        return userAdmin;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setUserAdmin(UserAdmin userAdmin) {
-        this.userAdmin = userAdmin;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public Employee getEmployee() {
-        return employee;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public void deleteCitizen (Citizen citizen) {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean approve() {
         //TODO
-    }
-
-    public void modifyCitizen (Citizen citizen) {
-        //TODO
-    }
-
-    public void insertRole () {
-        //TODO
-    }
-
-    public void deleteRole() {
-        //TODO
-    }
-
-    public void modifyRole() {
-        //TODO
+        return true;
     }
 }
---
+++ src/main/java/com/hua/gr/DS_Assignment/UserController.java
@@ -0,0 +1,95 @@
+package com.hua.gr.DS_Assignment;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    //user/register?firstName=[firstName]&lastName=[lastName]&email=[email]&password=[password]
+    @GetMapping("/user/register")
+    public void register(@RequestParam String firstName, @RequestParam String lastName,
+                      @RequestParam String email, @RequestParam String password){
+
+        UserCatalog newUser = new UserCatalog();
+        newUser.setFirstName(firstName);
+        newUser.setLastName(lastName);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        newUser.setPermission(0);
+
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(UserCatalog.class).buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+
+            session.save(newUser);
+
+            session.getTransaction().commit();
+
+
+        }finally {
+            factory.close();
+        }
+
+    }
+
+    @GetMapping("/user/login")
+    public void login(@RequestParam String email, @RequestParam String password){
+
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(UserCatalog.class).buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+
+        try {
+
+            session.beginTransaction();
+
+            String query1 = "from UserCatalog u where u.email= :email and u.password= :pass";
+            Query finalQuery = session.createQuery(query1);
+            finalQuery.setParameter("email", email);
+            finalQuery.setParameter("pass", password);
+
+            List<UserCatalog> search = finalQuery.getResultList();
+
+
+            for (UserCatalog u : search) {
+                System.out.println(u.getFirstName());
+            }
+
+            session.getTransaction().commit();
+
+
+        }finally {
+            factory.close();
+        }
+    }
+
+    @GetMapping("/user/delete")
+    public void delete(@RequestParam String email){
+
+    }
+
+
+    @GetMapping("/user/modify")
+    public void modify(@RequestParam String email){
+
+    }
+
+
+
+}
