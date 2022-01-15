7e936f0e270fa121e9077c0ba4279741a25fe3a3
Apos_Dimit <aposdimit@gmail.com>
2022-01-15 18:57:01 +0200

    Spring Security

---
+++ src/main/java/com/hua/gr/DS_Assignment/repository/UserCatalogRepository.java
@@ -0,0 +1,7 @@
+package com.hua.gr.DS_Assignment.repository;
+
+import com.hua.gr.DS_Assignment.UserCatalog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCatalogRepository extends JpaRepository<UserCatalog, String> {
+}
